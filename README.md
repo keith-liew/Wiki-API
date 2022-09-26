d82a9c9115a43edf4a032ca1d326d9e9648c08f2
keith-liew <>
2022-09-26 16:46:18 +0800

    Complete working API

---
+++ README.md
@@ -0,0 +1,75 @@
+# Wiki-API
+> A simple Express.js and MongoDB RESTful API
+
+<!-- Live demo [_here_](https://www.example.com).  If you have the project hosted somewhere, include the link here. -->
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Screenshots](#screenshots)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Acknowledgements](#acknowledgements)
+* [P.S.](#postscripts)
+
+
+## General Information
+- It is a practical project
+- The API is build according to the RESTful archietecture
+- It is created for self-education purpose only
+
+
+## Technologies Used
+- body-parser: ^1.20.0
+- express: ^4.18.1
+- mongoose: ^6.6.1
+
+
+## Features
+- Perform CRUD function to the database with HTTP requests
+
+
+## Screenshots
+<!-- ![Example screenshot](./img/screenshot.png) -->
+<!-- If you have screenshots you'd like to share, include them here. -->
+
+
+## Setup
+- Project environment:
+    - [Node.js](https://nodejs.org/en/) installed
+    - [MongoDB Shell](https://www.mongodb.com/docs/mongodb-shell/) installed
+
+
+## Usage
+- On project terminal
+    - install dependencies `npm install`
+    - start the app `npm app.js`
+- On Mongodb Shell
+    - start the database server `mongosh`
+
+
+## Project Status
+Project is: _incomplete_
+
+
+## Room for Improvement
+Room for improvement:
+- Modulize the models and routers
+- Return response with status code
+
+To do:
+- Continue development in order to further complete the API structure
+
+
+## Acknowledgements
+- This project was inspired by Angela Yu.
+- This project was based on [this course](https://www.udemy.com/course/the-complete-web-development-bootcamp/) on Udemy.
+- And also [this tutorial](https://www.youtube.com/watch?v=fgTGADljAeg) on Youtube.
+
+
+## Postscripts
+- A set of default json data is added for the purpose of create data at ease.
+- A .rest file is created for the purpose of create http requests at ease. It can only be used with the REST Client extension from Visual Studio Code 
---
+++ app.js
@@ -0,0 +1,73 @@
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+
+const Article = require("./Article");
+const defaultData = require("./defaultData.json");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikidb").then(console.log("Database connected")).catch(function (e) { console.log(e.message) });
+//insert default data if database is empty
+Article.countDocuments().then(function(result){
+    if(result === 0){
+        Article.insertMany(defaultData).then( console.log("Initialize database")).catch(function(e){console.log(e.message)});
+    }
+}).catch(function(e){console.log(e.message)});
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find({})
+            .then(function (results) { res.send(results) })
+            .catch(function (e) { res.send(e.message) });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        })
+        newArticle.save().then(res.send("Article saved")).catch(function (e) { res.send(e.message) });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany({}).then(res.send("Articles deleted")).catch(function (e) { res.send(e.message) });
+    });
+
+app.route("/articles/:query")
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.query })
+            .then(function (results) {
+                if (results) {
+                    res.send(results)
+                } else {
+                    res.send("Title not found");
+                }
+            }).catch(function (e) { res.send(e.message) });
+    })
+    .put(function (req, res) {
+        Article.replaceOne({ title: req.params.query }, { title: req.body.title, content: req.body.content }, null, function(e){
+            if(!e){
+                res.send("Article updated")
+            } else {
+                res.send(e.message)
+            }
+        })
+    })
+    .patch(function (req,res){
+        Article.updateOne({title: req.params.query},req.body, function(e){
+            if(!e){
+                res.send("Article updated")
+            } else {
+                res.send(e.message)
+            } 
+        });
+    })
+    .delete(function (req,res){
+        Article.deleteOne({title: req.params.query}).then(res.send("Article deleted")).catch(function (e) { res.send(e.message) });
+    });
+
+app.listen(3000, function () {
+    console.log("Server stared on port 3000");
+});
+
